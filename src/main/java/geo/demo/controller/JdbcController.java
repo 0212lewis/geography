57ed6781eeaa83a1d7d904b1f31afc3860c0c74f
chenyzh <151250021@smail.nju.edu.cn>
2019-05-14 12:56:02 +0800

    'all'

--- src/main/java/geo/demo/controller/JdbcController.java
+++ src/main/java/geo/demo/controller/JdbcController.java
@@ -5,13 +5,14 @@ import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.core.RowMapper;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import javax.annotation.Resource;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.List;
 
-@Controller
+@RestController
 @RequestMapping
 public class JdbcController {
 
